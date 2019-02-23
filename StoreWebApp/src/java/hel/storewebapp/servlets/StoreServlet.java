4755215ec69ae90ad94d4cc5d2efd91f3f78d609
Herilen <47165686+Herilen@users.noreply.github.com>
2019-02-22 20:02:05 -0600

    back

--- StoreWebApp/src/java/hel/storewebapp/servlets/StoreServlet.java
+++ StoreWebApp/src/java/hel/storewebapp/servlets/StoreServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hel.storewebapp.servlets;
 
 import java.io.IOException;
@@ -13,26 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author 15br101la
- */
+//clase con nombre "StoreServlet" 
 @WebServlet(name = "StoreServlet", urlPatterns = {"/StoreServlet"})
-public class StoreServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+public class StoreServlet extends HttpServlet 
+{
+    //metodo de la clase StoreServlet
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException 
+    {
+        //cuerpo del metodo
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) 
+        {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
